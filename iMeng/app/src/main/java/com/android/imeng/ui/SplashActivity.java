0869bd614fe9832b55627cb35a0d6d1c2d94777a
hiphonezhu <hiphonezhu@gmail.com>
2015-05-31 00:17:53 +0800

    添加闪屏功能

--- iMeng/app/src/main/java/com/android/imeng/ui/SplashActivity.java
+++ iMeng/app/src/main/java/com/android/imeng/ui/SplashActivity.java
@@ -3,9 +3,6 @@ package com.android.imeng.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-
 import com.android.imeng.R;
 import com.android.imeng.framework.ui.BasicActivity;
 import com.android.imeng.framework.ui.base.annotations.ViewInject;
@@ -23,27 +20,15 @@ public class SplashActivity extends BasicActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        AlphaAnimation alphaAnimation = new AlphaAnimation(1f, 0f);
-        alphaAnimation.setDuration(3000);
-        alphaAnimation.setFillAfter(true);
-        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+        // 延迟3秒, 跳转到首页
+        splashView.postDelayed(new Runnable() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                // 跳转到首页
+            public void run() {
                 startActivity(new Intent(SplashActivity.this, HomeActivity.class));
                 finish();
+                // 淡出淡入效果
+                overridePendingTransition(R.anim.fade_int, R.anim.fade_out);
             }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-        splashView.startAnimation(alphaAnimation);
+        }, 3000);
     }
 }
